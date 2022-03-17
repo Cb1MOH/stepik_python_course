bfa5b9325d8f62ca675cc979a83daa7937b81091
mchernyshou <maxchernyshou@gmail.com>
2022-03-17 23:04:16 +0100

    2.3 Task 1

---
+++ python_stepik/2.3.py
@@ -0,0 +1,14 @@
+# Task_1
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# print('', end='\t')
+# for g in range(c, d + 1):
+#     print(g, end='\t')
+# print('')
+# for i in range(a, b + 1):
+#     print(i, end='\t')
+#     for j in range(c, d + 1):
+#         print(i * j, end='\t')
+#     print('\t')
