cd775bac40655167bc2e7746cceb5ce939b19654
mchernyshou <maxchernyshou@gmail.com>
2022-03-17 22:26:28 +0100

    2.2 Task 1

---
+++ python_stepik/2.2.py
@@ -0,0 +1,10 @@
+# Task_1
+# number = 0
+# while number <= 100:
+#     number = int(input())
+#     if number < 10:
+#         continue
+#     elif number > 100:
+#         break
+#     else:
+#         print(number)
