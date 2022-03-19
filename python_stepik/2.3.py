8304b9e79ab7f6e462c2c562a0952103bed2943b
mchernyshou <maxchernyshou@gmail.com>
2022-03-19 15:57:36 +0100

    2.3 Task to count all odd numbers in a range

--- python_stepik/2.3.py
+++ python_stepik/2.3.py
@@ -12,3 +12,14 @@
 #     for j in range(c, d + 1):
 #         print(i * j, end='\t')
 #     print('\t')
+
+# Вывусти сумму всех нечетных чисел от a до b(включая обе границы)
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# sum = 0
+# for i in range(a, b+1):
+#     if i % 2 == 1:
+#         sum += i
+#     print(i)
+# print(sum)
