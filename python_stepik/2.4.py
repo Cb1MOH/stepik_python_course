0bf0940103fe6a5bb9a394db75a993a48e437f78
mchernyshou <maxchernyshou@gmail.com>
2022-03-19 20:27:54 +0100

    2.4 Task 2

---
+++ python_stepik/2.4.py
@@ -0,0 +1,26 @@
+# Task_1
+# a = input()
+# G = a.lower().count('g')
+# C = a.lower().count('c')
+# L = len(a)
+# result = (C + G) / L * 100
+# print(result)
+
+# Polindrome
+# inputString = input()
+# if inputString.lower() == inputString[::-1].lower():
+#     print('YES')
+# else:
+#     print('NO')
+
+# Task_2
+# s = input()
+# n = len(s)
+# count = 1
+# for i in range(n - 1):
+#     if s[i] == s[i + 1]:
+#         count += 1
+#     else:
+#         print(s[i] + str(count), end='')
+#         count = 1
+# print(s[-1] + str(count), end='')
