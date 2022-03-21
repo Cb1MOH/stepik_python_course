96c94bed6f8173996d2f5f69a052afc30ed5bbc4
mchernyshou <maxchernyshou@gmail.com>
2022-03-21 22:41:31 +0100

    2.5 Task 2

--- python_stepik/2.5.py
+++ python_stepik/2.5.py
@@ -9,3 +9,21 @@
 # for i in a:
 #     s += i
 # print(s)
+
+# Task_2
+# a = [int(i) for i in input().split()]
+# b = []
+# count = 1
+# c = ''
+# if len(a) == 1:
+#     c = str(a[0])
+#     print(c)
+# else:
+#     b.append(a[-1] + a[1])
+#     for i in a[1:-1]:
+#         b.append(a[count - 1] + a[count + 1])
+#         count += 1
+#     b.append(a[-2] + a[0])
+#     for i in b:
+#         c = c + str(i) + ' '
+#     print(c)
