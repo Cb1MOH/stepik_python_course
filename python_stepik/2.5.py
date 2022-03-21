0bb3f1d20a8039f0326a26e97916c192a8fcdba1
mchernyshou <maxchernyshou@gmail.com>
2022-03-21 21:41:22 +0100

    2.5 Task 1

---
+++ python_stepik/2.5.py
@@ -0,0 +1,11 @@
+# students = ['Ivan', 'Masha', 'Sasha']
+# students += ['Olga']
+# students += 'Olga'
+# print(students)
+
+# Task_1
+# a = [int(i) for i in input().split()]
+# s = 0
+# for i in a:
+#     s += i
+# print(s)
