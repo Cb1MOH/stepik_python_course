9251e066fed51b807e2fc197b04d614c65a6150c
mchernyshou <maxchernyshou@gmail.com>
2022-04-09 19:28:33 +0200

    3.1 Task 1 and 2

---
+++ python_stepik/3.1.py
@@ -0,0 +1,21 @@
+# Task_1
+# def f(x):
+#     if x <= -2:
+#         return 1 - (x + 2) ** 2
+#     elif x -2 < x <= 2:
+#         return (x * -1) / 2
+#     elif 2 < x:
+#         return (x - 2) ** 2 + 1
+
+# Task_2
+# def modify_list(l):
+#     for i in range(len(l))[::-1]:
+#         if l[i] % 2 == 0:
+#             l[i] //= 2
+#         else:
+#             del l[i]
+#
+#
+# lst = [10, 5, 8, 3]
+# modify_list(lst)
+# print(lst)
