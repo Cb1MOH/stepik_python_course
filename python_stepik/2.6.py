efc1556154eef8e88eda0d25561e7570ea1ba07e
mchernyshou <maxchernyshou@gmail.com>
2022-04-11 18:27:49 +0200

    2.6 Tasks

--- python_stepik/2.6.py
+++ python_stepik/2.6.py
@@ -62,3 +62,33 @@
 #     upd_lst.insert(indx, 0)
 #     del upd_lst[indx + 1]
 # print(*lst_of_indx)
+
+# Task_4
+# TODO finish the task https://stepik.org/lesson/3369/step/10?unit=952
+
+
+# Soper
+# n, m, k = 5, 5, 3
+# # # n, m, k = (int(i) for i in input().split())
+# a = [[0 for j in range(m)] for i in range(n)]
+# for i in range(k):
+#     row, col = (int(i) - 1 for i in input().split())
+#     a[row][col] = -1
+# for i in range(n):
+#     for j in range(m):
+#         if a[i][j] == 0:
+#             for di in range(-1, 2):
+#                 for dj in range(-1, 2):
+#                     ai = i + di
+#                     aj = j + dj
+#                     if 0 <= ai < n and 0 <= aj < m and a[ai][aj] == -1:
+#                         a[i][j] += 1
+# for i in range(n):
+#     for j in range(m):
+#         if a[i][j] == -1:
+#             print('*', end='')
+#         elif a[i][j] == 0:
+#             print('.', end='')
+#         else:
+#             print(a[i][j], end='')
+#     print()
