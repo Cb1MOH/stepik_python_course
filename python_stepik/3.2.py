05909b80f9a892de60d095a9466ee949ec28a98d
mchernyshou <maxchernyshou@gmail.com>
2022-07-12 00:37:58 +0200

    Task 1

--- python_stepik/3.2.py
+++ python_stepik/3.2.py
@@ -1,24 +1,35 @@
 # Task_1
-# Программа меняет по сути значение исходного словаря.
-# Если ключ key есть в словаре d, то ДОБАВЬТЕ значение value в СПИСОК, который хранится по этому ключу. - То есть, если key есть в d  то данному ключу ДОБАВЛЯЕТСЯ (не присваивается, подразумевается что под этим ключем что уже могло быть,  не нужно переприсваивать ему значения, нужно просто добавить) значение value в СПИСОК!.
-# Если ключа key нет в словаре, то нужно ДОБАВИТЬ значение в СПИСОК по ключу 2∗key.  . Если и ключа 2∗keyнет, то нужно ДОБАВИТЬ ключ 2∗key в словарь и СОПОСТАВИТЬ ему СПИСОК из переданного элемента [value].======
-# ==То есть, если ключа key нет в словаре, то нужно сначала проверить есть ли ключ 2*key в словаре и если он есть, то ДОБАВИТЬ значение value в СПИСОК. А если ключа 2*key в словаре нет,  то нужно создать это ключ 2*key и ему СОПОСТАВИТЬ = ПРИСВОИТЬ  ему значение value.
-# Для добавить интуитивно можно юзать +=
-
+# def update_dictionary(d, key, value):
+#     if key not in d:
+#         if d[2 * key] not in d:
+#             d[2 * key] = [value]
+#         else:
+#             d[2 * key] = [int(x) for x in str(d[2 * key])]
+#             d[2 * key].append(value)
+#     else:
+#         d[key] = [int(x) for x in str(d[key])]
+#         d[key].append(value)
 
 def update_dictionary(d, key, value):
     if key in d:
-        d[key] = value
-    elif key not in d:
-        if d[2 * key] not in d:
-            d[2 * key] = key
+        d[key].append(value)
+    else:
+        if (key * 2) in d:
+            d[key * 2].append(value)
         else:
-            d[2 * key] = [value]
+            d[key * 2] = [value]
 
 
+d = {}
+print(update_dictionary(d, 1, -1))  # None
+print(d)                            # {2: [-1]}
+update_dictionary(d, 2, -2)
+print(d)                            # {2: [-1, -2]}
+update_dictionary(d, 1, -3)
+print(d)                            # {2: [-1, -2, -3]}
 
-
-d = {1: 11, 2: 22, 3:[]}
-
-update_dictionary(d, 33, 11)
-print(d)
+# di = {"one": 1, "two": 2, "three": 3}
+# di["one"] = [int(x) for x in str(di["one"])]
+# di["one"].append(2)
+#
+# print(di)
