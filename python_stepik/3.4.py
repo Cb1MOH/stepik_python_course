b791a674aa5fafe4b4bf3cb75af23172c95a1e13
mchernyshou <maxchernyshou@gmail.com>
2022-07-13 01:10:11 +0200

    Task 2

---
+++ python_stepik/3.4.py
@@ -0,0 +1,27 @@
+# Task 1
+# with open("data/dataset_3363_2.txt") as text:
+#     s1 = text.readline()
+#     general = ""
+#     char = ""
+#     num = ""
+#     num_list = []
+#     index = 0
+#
+#     for i in s1:
+#         if i.isalpha():
+#             char += i
+#             num += " "
+#         else:
+#             char += " "
+#             num += i
+#     char = char.split()
+#     num = num.split()
+#
+#     for i in num:
+#         num_list.append(int(i))
+#
+#     for i in char:
+#         general += i * num_list[index]
+#         index += 1
+#
+#     print(general)
