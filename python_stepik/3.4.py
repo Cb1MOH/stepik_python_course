7b8355e956c07a7ac30aca4b083ef4101cd87fa2
mchernyshou <maxchernyshou@gmail.com>
2022-07-13 17:11:06 +0200

    Task 3

--- python_stepik/3.4.py
+++ python_stepik/3.4.py
@@ -25,24 +25,46 @@
 #         index += 1
 #     print(general)
 
-# Task 3
-formatted_body = ""
-words_list = []
-count_words = {}
-with open("data/dataset_3363_3.txt") as text:
-    body = text.read()
-    for i in body:
-        if i in "!@#$%^&*(){}:\"?><.,":
-            formatted_body += ""
-        else:
-            formatted_body += i
-    words_list = formatted_body.lower().split()
-    for word in words_list:
-        if word in count_words:
-            count_words[word] += 1
-        else:
-            count_words[word] = 1
-    find_max = max(count_words, key=count_words.get)
+# Task 2
+# formatted_body = ""
+# words_list = []
+# count_words = {}
+# with open("data/dataset_3363_4.txt") as text:
+#     body = text.read()
+#     for i in body:
+#         if i in "!@#$%^&*(){}:\"?><.,":
+#             formatted_body += ""
+#         else:
+#             formatted_body += i
+#     words_list = formatted_body.lower().split()
+#     for word in words_list:
+#         if word in count_words:
+#             count_words[word] += 1
+#         else:
+#             count_words[word] = 1
+#     find_max = max(count_words, key=count_words.get)
+#
+#     print(find_max + " " + str(count_words[find_max]))
+#     print(words_list)
 
-    print(find_max + " " + str(count_words[find_max]))
-    print(words_list)
+# Task 3
+# with open("data/dataset_3363_4.txt", "r", encoding="utf-8") as text:
+#     people_and_marks = {}
+#     readline = text.read().splitlines()
+#     for i in readline:
+#         split_line = i.split(";")
+#         people_and_marks[split_line[0]] = [split_line[1], split_line[2], split_line[3]]
+#     for i in people_and_marks.keys():
+#         print((int(people_and_marks[i][0]) + int(people_and_marks[i][1]) + int(people_and_marks[i][2])) / 3)
+#     students_quantity = len(people_and_marks.keys())
+#     math = 0
+#     phiz = 0
+#     rus = 0
+#     x = 0
+#     for i in people_and_marks.values():
+#         math += int(i[0])
+#         phiz += int(i[1])
+#         rus += int(i[2])
+#     print(math / students_quantity, end=" ")
+#     print(phiz / students_quantity, end=" ")
+#     print(rus / students_quantity, end=" ")
