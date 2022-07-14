6c37b4f8dce8f88e0d342a8f387875bc1433295f
mchernyshou <maxchernyshou@gmail.com>
2022-07-14 19:26:08 +0200

    Task 2 remove comments

--- python_stepik/3.6.py
+++ python_stepik/3.6.py
@@ -33,27 +33,3 @@ import requests
 # print(result)
 
 
-#
-# name = text1
-# file1 = base_url + name
-# r2 = requests.get(file1)
-# text2 = r2.text
-# print(text2)
-#
-# name = text2
-# file2 = base_url + name
-# r3 = requests.get(file2)
-# text3 = r3.text
-# print(text3)
-#
-# name = text3
-# file4 = base_url + name
-# r4 = requests.get(file4)
-# text4 = r4.text
-# print(text4)
-#
-# name = text4
-# file5 = base_url + name
-# r5 = requests.get(file5)
-# text5 = r5.text
-# print(text5)
