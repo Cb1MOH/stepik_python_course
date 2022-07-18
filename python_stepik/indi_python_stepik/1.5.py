cba26bce5d3bf31151a1eb16a548382754564280
mchernyshou <maxchernyshou@gmail.com>
2022-07-18 19:12:13 +0200

    Tasks for 1.5 class

---
+++ python_stepik/indi_python_stepik/1.5.py
@@ -0,0 +1,63 @@
+# a = int(input())
+# print(a + 1)
+
+# a, b = map(int, input().split())
+# print(a + b)
+
+# S = int(input())
+# p = int(S / 6)
+# s = p
+# k = (p + s) * 2
+# print(str(p) + " " + str(k) + " " + str(s))
+
+# pencil, pen, marker = map(int, input().split())
+# pencil_price = 3
+# pen_price = pencil_price + 2
+# marker_price = pen_price + 7
+# print(pencil_price * pencil + pen_price * pen + marker_price * marker)
+
+
+# N, A, B = map(int, input().split())
+# result = ((A * B) * N) * 2
+# print(result)
+
+
+# a, b, c, d = map(int, input().split())
+# print((a + b + c + d) / 4)
+
+# start_H = int(input())
+# start_M = int(input())
+# start_S = int(input())
+# end_H = int(input())
+# end_M = int(input())
+# end_S = int(input())
+# start_time = start_H * 60 * 60 + start_M * 60 + start_S
+# end_time = end_H * 60 * 60 + end_M * 60 + end_S
+# print(end_time - start_time)
+
+# harry, larry = map(int, input().split())
+# print(str(larry - 1) + " " + str(harry - 1))
+
+# a = int(input())
+# b = int(input())
+# print(abs(a) + abs(b))
+
+# a, b = map(float, input().split())
+# print(max(a, b) - min(a, b))
+
+# a, b, c, d, e = map(int, input().split())
+# print(max(a, b, c, d, e))
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# case1 = a + b + c
+# case2 = a + b * c
+# case3 = a * b + c
+# case4 = a * b * c
+# case5 = (a + b) * c
+# case6 = a * (b + c)
+# print(max(case1, case2, case3, case4, case5, case6))
+
+# a = float(input())
+# print(str(round(a, 2)) + " " + str(round(a, 3)))
