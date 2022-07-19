a8749781e2f57e136f54a9f328eb39736668078c
mchernyshou <maxchernyshou@gmail.com>
2022-07-19 16:40:28 +0200

    1.7 partial task

---
+++ python_stepik/indi_python_stepik/1.7.py
@@ -0,0 +1,23 @@
+# a = int(input())
+# print(a//1000)
+
+# n = int(input())
+# k = int(input())
+# print(k//n)
+
+# N = int(input())
+# K = int(input())
+# print(N//K)
+
+# a = int(input())
+# print(a % 10)
+
+# a = int(input())
+# print(a//100%10)
+
+# a = int(input())
+# print(a // 100 + a // 10 % 10 + a // 1 % 10)
+
+# a = int(input())
+a = 476
+print(a//100 + a //100%20 + a //10%10 + a//1 %5 + a//1 %1)
