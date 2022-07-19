dce989655cd59c45a7dbe9b4221ca20d13d2f563
mchernyshou <maxchernyshou@gmail.com>
2022-07-19 15:50:22 +0200

    1.6 Tasks

---
+++ python_stepik/indi_python_stepik/1.6.py
@@ -0,0 +1,21 @@
+# print("Привет, Мир!")
+
+# a, b, c = map(int, input().split())
+# print(a, b, c, sep=",")
+
+# a = int(input())
+# print(a - 1, a, a + 1, sep="<")
+
+# a = input()
+# b = input()
+# c = input()
+# print(a, b, c, sep="---")
+
+# separator = input()
+# print(1, 2, 3, 4, 5, sep=separator)
+
+# a = input()
+# print(a, end=" - Сказала она!")
+
+# print('Гвидо', 'Ван', 'Россум', sep='*', end='-')
+# print('Основатель', 'Питона', sep='_', end='!')
