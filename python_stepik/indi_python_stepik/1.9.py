786ca17b9b34591c5bdd6c4c9f2216a58a039ec5
mchernyshou <maxchernyshou@gmail.com>
2022-07-30 20:23:19 +0200

    1.9 tasks

---
+++ python_stepik/indi_python_stepik/1.9.py
@@ -0,0 +1,36 @@
+# a = int(input())
+# print(a > 0)
+
+# a = int(input())
+# print(a % 2 == 0)
+
+# a = int(input())
+# print(a % 6 == 0)
+
+# a = int(input())
+# print(a % 9 != 0)
+
+# a = int(input())
+# print(a % 10 == 2)
+
+# a, b = map(int, input().split())
+# print(a % 7 == 0 and b % 7 == 0)
+
+# a, b, c = map(int, input().split())
+# print(a == b == c)
+
+# a = int(input())
+# print(5 < a <= 19)
+
+# a = input()
+# b = input()
+# print(a == 'awesome' or b == 'awesome')
+
+# a, b, c = map(int, input().split())
+# print(a == b or b == c or c == a)
+
+# a = int(input())
+# print(10 <= a <= 99)
+
+# a, b, c = map(int, input().split())
+# print(a ** 2 + b ** 2 == c ** 2 or a ** 2 + c ** 2 == b**2 or c ** 2 + b ** 2 == a ** 2)
