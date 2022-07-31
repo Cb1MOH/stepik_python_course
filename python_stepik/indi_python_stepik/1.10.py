d3561abf19ffa546dde48690e4b99018389cb531
mchernyshou <maxchernyshou@gmail.com>
2022-07-31 21:18:04 +0200

    1.10 tasks

---
+++ python_stepik/indi_python_stepik/1.10.py
@@ -0,0 +1,24 @@
+# import math
+
+# a = int(input())
+# b = a / 10
+# print(math.ceil(b))
+
+
+# import math
+#
+# a = int(input())
+# print(math.ceil(a/4))
+# import math
+#
+# a = int(input())
+# b = int(input())
+# c = int(input())
+#
+# print(math.ceil(a/2) + math.ceil(b/2) + math.ceil(c/2))
+
+
+# from math import ceil
+#
+# l, w, h = map(int, input().split())
+# print(ceil(((w * h + l * h) * 2) / 16))
