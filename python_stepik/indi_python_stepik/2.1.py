9eb62f9ba627941a2a647439b945540609fc75a6
mchernyshou <maxchernyshou@gmail.com>
2022-08-09 20:09:41 +0200

    2.1 tasks

---
+++ python_stepik/indi_python_stepik/2.1.py
@@ -0,0 +1,31 @@
+# print("Я стану крутым программистом!\n" * 3)
+
+# print("W" * 777)
+
+# a = input()
+# b = input()
+# print(a, b, sep="\n")
+
+
+# a = input()
+# b = input()
+# c = input()
+# print(c, b, a, sep="\n")
+
+# a = input()
+# print((a + " " )* 4)
+
+# a = input()
+# print(len(a))
+
+# a = input()
+# b = input()
+# print(b + a)
+
+# a = input()
+# print(a * 3)
+
+# a, b, c = map(str, input().split())
+# print("Simvol code " + a + " is " + str(ord(a)) + ".")
+# print("Simvol code " + b + " is " + str(ord(b)) + ".")
+# print("Simvol code " + c + " is " + str(ord(c)) + ".")
