0dc64c1caa31190d1bc84b1050d353b2eb623d11
mchernyshou <maxchernyshou@gmail.com>
2022-09-22 15:42:44 +0200

    tasks 2.7, 2.8 and 2.9

--- python_stepik/indi_python_stepik/2.7.py
+++ python_stepik/indi_python_stepik/2.7.py
@@ -3,7 +3,46 @@
 # print(result)
 
 
-name = input()
-age = int(input())
-result = f"Hello {name.upper()}. You are {age} years old."
+# name = input()
+# age = int(input())
+# result = f"Hello {name.upper()}. You are {age} years old."
+# print(result)
+
+# name = input()
+# age = int(input())
+# year = age + 77
+# result = f"{name}, вам исполнится 77 лет в {year}"
+# print(result)
+
+# sec = int(input())
+# minutes = sec // 60
+# seconds = sec % 60
+# result = f"{sec} сек - это {minutes} мин. {seconds} сек."
+# print(result)
+
+# a, b = map(int, input().split())
+# result = f"""Разрешение экрана: {a} x {b}.
+# Общее количество пикселей = {a * b}."""
+# print(result)
+
+# a = int(input())
+# b = int(input())
+# result = f"""{a} / {b} = {a / b}
+# {a} // {b} = {a // b}
+# {a} % {b} = {a % b}"""
+# print(result)
+
+
+# x = int(input())
+# y = int(input())
+# z = int(input())
+# result = f"""Vector A({x}, {y}, {z})
+# Vector B({x + 5}, {y + 5}, {z + 5})"""
+# print(result)
+
+
+rate = float(input())
+dollars = int(input())
+result = f"""Current dollar rate is {rate}. You want buy {dollars} dollars
+You must pay {rate * dollars}"""
 print(result)
---
+++ python_stepik/indi_python_stepik/2.8.py
@@ -0,0 +1,38 @@
+# x = int(input())
+# y = int(input())
+# result = f"Точка(x = {x}, y = {y})"
+# print(result)
+
+# number_pi = 3.141592653589793
+# print(f'{number_pi:.6f}')
+
+# number = float(input())
+# print(f"{number:.2f}")
+
+
+# x = int(input())
+# print(f"{x:010d}")
+
+#Пример выравнивания
+# number = 12345.6789
+# print(f"Число {number = }")
+# print(f"|{number:25}|")
+# print(f"|{number:<25}|")
+# print(f"|{number:>25}|")
+# print(f"|{number:^25}|")
+# print('-'*25)
+# text = "Python 3.10"
+# print(f"Строка {text = }")
+# print(f"|{text:25}|")
+# print(f"|{text:<25}|")
+# print(f"|{text:>25}|")
+# print(f"|{text:^25}|")
+
+
+# number = int(input())
+# print(f"{number: ^15}")
+
+# text = input()
+# print(f"|{text:&^20}|")
+# print(f"|{text:&>20}|")
+# print(f"|{text:&<20}|")
